6f3598153bf1e9915d36097e0240d7c29d64fb6f
samuelmoiwa <samuelmoiwa@gmail.com>
2020-04-10 01:59:28 +0000

    simple flash light app..

---
+++ app/src/main/java/com/njalauniversity/torchflashlight/MainActivity.java
@@ -0,0 +1,117 @@
+package com.njalauniversity.torchflashlight;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button buttonEnable;
+    private ImageView imageFlashlight;
+    private static final int CAMERA_REQUEST = 50;
+    private boolean flashLightStatus = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imageFlashlight = findViewById(R.id.imageFlashlight);
+        buttonEnable = findViewById(R.id.buttonEnable);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+        final boolean hasCameraFlash = getPackageManager().
+                hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        boolean isEnabled = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
+
+        buttonEnable.setEnabled(!isEnabled);
+        imageFlashlight.setEnabled(isEnabled);
+        buttonEnable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST);
+            }
+        });
+
+        imageFlashlight.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                if (hasCameraFlash) {
+                    if (flashLightStatus)
+                        flashLightOff();
+                    else
+                        flashLightOn();
+                } else {
+                    Toast.makeText(MainActivity.this, "No flash available on your device",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void flashLightOn() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, true);
+            flashLightStatus = true;
+            imageFlashlight.setImageResource(R.mipmap.on);
+        } catch (CameraAccessException e) {
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void flashLightOff() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, false);
+            flashLightStatus = false;
+            imageFlashlight.setImageResource(R.mipmap.off);
+        } catch (CameraAccessException e) {
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case CAMERA_REQUEST :
+                if (grantResults.length > 0  &&  grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    buttonEnable.setEnabled(false);
+                    buttonEnable.setText("Camera Enabled");
+                    imageFlashlight.setEnabled(true);
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied for the Camera", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    }
+
